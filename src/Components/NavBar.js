7a7bbaf5b644d2c10a7f23b05230707e10324134
Sydney Spaulding <cozysydmedia@gmail.com>
2024-03-09 13:53:40 -0800

    edited nav links

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -7,28 +7,34 @@ function NavBar() {
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Cozy Syd's Library</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
+            <Nav.Link href="#reviews">Recent Reviews</Nav.Link>
+            <Nav.Link href="#pricing">Top Picks</Nav.Link>
+            <NavDropdown
+              title="Monthly Book Club"
+              id="collapsible-nav-dropdown"
+            >
+              <NavDropdown.Item href="#action/3.1">Romance</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Fantasy</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                Mystery/Thriller
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
-                Separated link
+                About Book Club
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
           <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
+            <Nav.Link href="#deets">Blog</Nav.Link>
+            <Nav.Link
+              eventKey={2}
+              href="https://www.goodreads.com/user/show/5441778-sydney"
+            >
+              Goodreads
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
