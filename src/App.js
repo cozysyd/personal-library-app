275a77e30d3ad6237d64aee384a07d8c01d2f3dd
Sydney Spaulding <cozysydmedia@gmail.com>
2024-03-08 20:25:13 -0800

    edited text

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ function App() {
           {books.length > 0 ? (
             <Books books={books} onDelete={deleteBook} onEdit={editBook} />
           ) : (
-            "No Book Found!"
+            "No Books Found!"
           )}
         </div>
       )}
