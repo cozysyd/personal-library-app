8e23713c51a4dca2947c0cf8661ee4cb4c389c0c
Sydney Spaulding <cozysydmedia@gmail.com>
2024-03-09 16:01:47 -0800

    debugged code

--- src/App.js
+++ src/App.js
@@ -11,19 +11,21 @@ import { Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  <div>
-    <NavBar />
-    <div className="container">
-      <Routes>
-        <Route path="/Home" element={<Home />} />
-        <Route path="/recentreviews" element={<RecentReviews />} />
-        <Route path="/favbooks" element={<FavBooks />} />
-        <Route path="/aboutbc" element={<AboutBC />} />
-        <Route path="/romancebc" element={<RomanceBC />} />
-        <Route path="/fantasybc" element={<FantasyBC />} />
-        <Route path="/thrillerbc" element={<ThrillerBC />} />
-      </Routes>
+  return (
+    <div>
+      <NavBar />
+      <div className="container">
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/recentreviews" element={<RecentReviews />} />
+          <Route path="/favbooks" element={<FavBooks />} />
+          <Route path="/aboutbc" element={<AboutBC />} />
+          <Route path="/romancebc" element={<RomanceBC />} />
+          <Route path="/fantasybc" element={<FantasyBC />} />
+          <Route path="/thrillerbc" element={<ThrillerBC />} />
+        </Routes>
+      </div>
     </div>
-  </div>;
+  );
 }
 export default App;
